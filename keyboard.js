d39683891c93d3d77ba1441f437349729a8b8958
vczero <wlhmyit@126.com>
2015-04-22 22:44:39 +0800

    add readme

--- keyboard.js
+++ keyboard.js
@@ -1,18 +1,24 @@
 
 ;(function(exports){
 	var KeyBoard = function(input, options){
+		var body = document.getElementsByTagName('body')[0];
+		var DIV_ID = options && options.divId || '__w_l_h_v_c_z_e_r_o_divid';
+		if(document.getElementById(DIV_ID)){
+			body.removeChild(document.getElementById(DIV_ID));
+		}
+		
 		this.input = input;
 		this.el = document.createElement('div');
+		
 		var self = this;
 		var zIndex = options && options.zIndex || 1000;
 		var width = options && options.width || '100%';
 		var height = options && options.height || '193px';
 		var fontSize = options && options.fontSize || '12px';
 		var backgroundColor = options && options.backgroundColor || '#fff';
-		var body = document.getElementsByTagName('body')[0];
+		var TABLE_ID = options && options.table_id || 'table_0909099';
 		
-		var TABLE_ID = 'table_0909099';
-		
+		this.el.id = DIV_ID;
 		this.el.style.position = 'absolute';
 		this.el.style.left = 0;
 		this.el.style.right = 0;
@@ -32,7 +38,7 @@
 		//Button
 		var btnStr = '<div style="width:60px;height:28px;background-color:#1FB9FF;';
 		btnStr += 'float:right;margin-right:5px;text-align:center;color:#fff;';
-		btnStr += 'line-height:28px;border-radius:3px;margin-bottom:5px;">完成</div>';
+		btnStr += 'line-height:28px;border-radius:3px;margin-bottom:5px;cursor:pointer;">完成</div>';
 		
 		//table
 		var tableStr = '<table id="' + TABLE_ID + '" border="0" cellspacing="0" cellpadding="0">';
@@ -53,12 +59,12 @@
 					self.input.value += value;
 				}
 			}else if(clickEl.tagName.toLocaleLowerCase() === 'div' && value === "完成"){
-				self.el.style.display = 'none';
+				body.removeChild(self.el);
 			}else if(clickEl.tagName.toLocaleLowerCase() === 'td' && value === "删除"){
 				var num = self.input.value;
 				if(num){
 					var newNum = num.substr(0, num.length - 1);
-					 self.input.value = newNum;
+					self.input.value = newNum;
 				}
 			}
 		}
@@ -66,12 +72,6 @@
 		this.el.onclick = addEvent;
 		body.appendChild(this.el);
 	}
-	
-	
-	KeyBoard.prototype.show = function(){
-		
-		this.el.style.display = 'block';
-	};
 	
 	exports.KeyBoard = KeyBoard;
 
